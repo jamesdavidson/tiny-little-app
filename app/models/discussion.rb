0785641eebca18be8e742ef773817e8ddb64d679
James Davidson <jsdavo@icloud.com>
2015-02-09 20:58:31 +1100

    Adds the current_{topic,location,beverage} methods and a bit to the view

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,4 +3,40 @@ class Discussion < ActiveRecord::Base
   has_many :topics, :through => :updates, :source => :updatable, :source_type => 'Topic'
   has_many :locations, :through => :updates, :source => :updatable, :source_type => 'Location'
   has_many :beverages, :through => :updates, :source => :updatable, :source_type => 'Beverage'
+
+  def current_topic
+    result = nil
+    self.updates.where(updatable_type: 'Topic').each do |u|
+      if u.retraction?
+        result = nil
+      else
+        result = u.updatable
+      end
+    end
+    result
+  end
+
+  def current_location
+    result = nil
+    self.updates.where(updatable_type: 'Location').each do |u|
+      if u.retraction?
+        result = nil
+      else
+        result = u.updatable
+      end
+    end
+    result
+  end
+
+  def current_beverage
+    result = nil
+    self.updates.where(updatable_type: 'Beverage').each do |u|
+      if u.retraction?
+        result = nil
+      else
+        result = u.updatable
+      end
+    end
+    result
+  end
 end
