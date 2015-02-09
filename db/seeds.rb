92bde96ad25b971108a400bc521ff0d86ec46f1e
James Davidson <jsdavo@icloud.com>
2015-02-09 20:06:00 +1100

    Adds some db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Discussion.create(shortlink: 'a123x4')
+[Discussion,Update,Topic,Location,Beverage].each(&:delete_all)
+
+d = Discussion.create(shortlink: 'a123x4')
+Update.create(discussion: d)
+Update.create(discussion: d)
+Update.create(discussion: d)
+
+# Update.create(discussion: d, updatable: Topic.create(name: 'Programming'))
+# Update.create(discussion: d, updatable: Location.create(name: 'Melbourne'))
+# Update.create(discussion: d, updatable: Beverage.create(name: 'Coffee'))
