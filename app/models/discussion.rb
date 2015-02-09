410745405291c74b64c92d918bd6bcc8aa8f3fc7
James Davidson <jsdavo@icloud.com>
2015-02-09 22:05:59 +1100

    Adds some handy associations
    This reverts commit 543f93294e9f23e3a99ecf81cf3066397969c2b2.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,5 +1,8 @@
 class Discussion < ActiveRecord::Base
   has_many :updates
+  has_many :topics, :through => :updates, :source => :updatable, :source_type => 'Topic'
+  has_many :locations, :through => :updates, :source => :updatable, :source_type => 'Location'
+  has_many :beverages, :through => :updates, :source => :updatable, :source_type => 'Beverage'
 
   # define the current_topic, current_location and current_beverage methods
   ['topic','location','beverage'].each do |thing|
