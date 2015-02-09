804c2ca9ad1b0a0ac8168da5d51d347ee3f2f3ab
James Davidson <jsdavo@icloud.com>
2015-02-09 20:26:48 +1100

    Adds the hard associations

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,6 @@
 class Discussion < ActiveRecord::Base
   has_many :updates
+  has_many :topics, :through => :updates, :source => :updatable, :source_type => 'Topic'
+  has_many :locations, :through => :updates, :source => :updatable, :source_type => 'Location'
+  has_many :beverages, :through => :updates, :source => :updatable, :source_type => 'Beverage'
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,3 +1,4 @@
 class Update < ActiveRecord::Base
   belongs_to :discussion
+  belongs_to :updatable, :polymorphic => :true
 end
