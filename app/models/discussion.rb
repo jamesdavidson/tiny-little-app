01554a3d26f0935fa6befb870315328c9fa70489
James Davidson <jsdavo@icloud.com>
2015-02-09 21:24:11 +1100

    Removes unused assocations

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,8 +1,5 @@
 class Discussion < ActiveRecord::Base
   has_many :updates
-  has_many :topics, :through => :updates, :source => :updatable, :source_type => 'Topic'
-  has_many :locations, :through => :updates, :source => :updatable, :source_type => 'Location'
-  has_many :beverages, :through => :updates, :source => :updatable, :source_type => 'Beverage'
 
   ['topic','location','beverage'].each do |thing|
     class_eval "def current_#{thing}
